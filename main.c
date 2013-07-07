14b7c06270dcb49a1e9858d67a1c3e589055bd1e
c-lab <coon@c-base.org>
2013-07-07 09:57:52 +0200

    crashes over crashes... there is a big memory problem. maybe too memory consuming or any other leak...

---
+++ main.c
@@ -0,0 +1,678 @@
+/*********************************************************************
+ *
+ * Code testing the basic functionality of STM32 on VL discovery kit
+ * The code displays message via UART1 using printf mechanism
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ *
+ *********************************************************************
+ * FileName:    main.c
+ * Depends:
+ * Processor:   STM32F100RBT6B
+ *
+ * Author               Date       Comment
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Kubik                13.11.2010 Initial code
+ * Kubik                14.11.2010 Added debug code
+ * Kubik                15.11.2010 Debug now goes to UART2
+ * Kubik                ??.11.2010 Added test code for quadrature encoder
+ * Kubik                 4.12.2010 Implemented SD/MMC card support
+ ********************************************************************/
+
+// important:
+//
+// - commented out int fputc(int ch, FILE * f) in printf.c and redefined in main.h to get printf sending directly to USART1
+// - commented out struct _reent *_impure_ptr = &r; in printf.c to prevent a linker error
+
+
+//-------------------------------------------------------------------
+// Includes
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "stm32f10x.h"
+#include "STM32_Discovery.h"
+#include "debug.h"
+#include "uart.h"
+#include "diskio.h"
+#include "ff.h"
+#include "midifile.h"
+
+
+FIL midi_file;
+
+
+void HexList(BYTE *pData, int iNumBytes)
+{
+	int i;
+
+	for(i=0;i<iNumBytes;i++)
+		printf("%.2x ", pData[i]);
+}
+
+
+void playMidiFile(const char *pFilename)
+{
+	_MIDI_FILE pMF;
+	BOOL open_success;
+	char str[128];
+	int ev;
+
+
+
+	FRESULT res = f_open (&midi_file, pFilename, FA_READ);
+	midiFileOpen(&midi_file, &pMF, pFilename, &open_success);
+
+	if (open_success)
+	{
+		MIDI_MSG msg;
+		int i, iNum;
+		unsigned int j;
+		int any_track_had_data;
+
+		midiReadInitMessage(&msg);
+		iNum = midiReadGetNumTracks(&pMF);
+
+
+		any_track_had_data = 1;
+
+		while(any_track_had_data)
+		{
+			any_track_had_data = 0;
+
+			for(i=0;i < iNum;i++)
+			{
+				if(midiReadGetNextMessage(&pMF, i, &msg)) // maybe this function will be the hardest part
+				{
+					any_track_had_data = 1;
+
+					printf("[Track: %d]", i);
+
+					if (msg.bImpliedMsg)
+					{ ev = msg.iImpliedMsg; }
+					else
+					{ ev = msg.iType; }
+
+					/*
+					if(ev != msgMetaEvent)
+						continue;
+
+					if(msg.MsgData.MetaEvent.iType != metaLyric)
+						continue;
+					*/
+
+
+					printf(" %.6ld ", msg.dwAbsPos);
+
+
+					if (muGetMIDIMsgName(str, ev))
+						printf("%s\t", str);
+
+					switch(ev)
+					{
+					case	msgNoteOff:
+						muGetNameFromNote(str, msg.MsgData.NoteOff.iNote);
+						printf("(%.2d) %s", msg.MsgData.NoteOff.iChannel, str);
+						break;
+					case	msgNoteOn:
+						muGetNameFromNote(str, msg.MsgData.NoteOn.iNote);
+						printf("\t(%.2d) %s %d", msg.MsgData.NoteOn.iChannel, str, msg.MsgData.NoteOn.iVolume);
+						break;
+					case	msgNoteKeyPressure:
+						muGetNameFromNote(str, msg.MsgData.NoteKeyPressure.iNote);
+						printf("(%.2d) %s %d", msg.MsgData.NoteKeyPressure.iChannel,
+							str,
+							msg.MsgData.NoteKeyPressure.iPressure);
+						break;
+					case	msgSetParameter:
+						muGetControlName(str, msg.MsgData.NoteParameter.iControl);
+						printf("(%.2d) %s -> %d", msg.MsgData.NoteParameter.iChannel,
+							str, msg.MsgData.NoteParameter.iParam);
+						break;
+					case	msgSetProgram:
+						muGetInstrumentName(str, msg.MsgData.ChangeProgram.iProgram);
+						printf("(%.2d) %s", msg.MsgData.ChangeProgram.iChannel, str);
+						break;
+					case	msgChangePressure:
+						muGetControlName(str, msg.MsgData.ChangePressure.iPressure);
+						printf("(%.2d) %s", msg.MsgData.ChangePressure.iChannel, str);
+						break;
+					case	msgSetPitchWheel:
+						printf("(%.2d) %d", msg.MsgData.PitchWheel.iChannel,
+							msg.MsgData.PitchWheel.iPitch);
+						break;
+
+					case	msgMetaEvent:
+						printf("---- ");
+						switch(msg.MsgData.MetaEvent.iType)
+						{
+						case	metaMIDIPort:
+							printf("MIDI Port = %d", msg.MsgData.MetaEvent.Data.iMIDIPort);
+							break;
+						case	metaSequenceNumber:
+							printf("Sequence Number = %d", msg.MsgData.MetaEvent.Data.iSequenceNumber);
+							break;
+						case	metaTextEvent:
+							printf("Text = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaCopyright:
+							printf("Copyright = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaTrackName:
+							printf("Track name = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaInstrument:
+							printf("Instrument = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaLyric:
+							printf("Lyric = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaMarker:
+							printf("Marker = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaCuePoint:
+							printf("Cue point = '%s'", msg.MsgData.MetaEvent.Data.Text.pData);
+							break;
+						case	metaEndSequence:
+							printf("End Sequence");
+							break;
+						case	metaSetTempo:
+							printf("Tempo = %d", msg.MsgData.MetaEvent.Data.Tempo.iBPM);
+							break;
+						case	metaSMPTEOffset:
+							printf("SMPTE offset = %d:%d:%d.%d %d",
+								msg.MsgData.MetaEvent.Data.SMPTE.iHours,
+								msg.MsgData.MetaEvent.Data.SMPTE.iMins,
+								msg.MsgData.MetaEvent.Data.SMPTE.iSecs,
+								msg.MsgData.MetaEvent.Data.SMPTE.iFrames,
+								msg.MsgData.MetaEvent.Data.SMPTE.iFF
+								);
+							break;
+						case	metaTimeSig:
+							printf("Time sig = %d/%d", msg.MsgData.MetaEvent.Data.TimeSig.iNom,
+								msg.MsgData.MetaEvent.Data.TimeSig.iDenom/MIDI_NOTE_CROCHET);
+							break;
+						case	metaKeySig:
+							if (muGetKeySigName(str, msg.MsgData.MetaEvent.Data.KeySig.iKey))
+								printf("Key sig = %s", str);
+							break;
+
+						case	metaSequencerSpecific:
+							printf("Sequencer specific = ");
+							HexList(msg.MsgData.MetaEvent.Data.Sequencer.pData, msg.MsgData.MetaEvent.Data.Sequencer.iSize);
+							printf("\n");
+							break;
+						}
+						break;
+
+					case	msgSysEx1:
+					case	msgSysEx2:
+						printf("Sysex = ");
+						HexList(msg.MsgData.SysEx.pData, msg.MsgData.SysEx.iSize); // ok
+						break;
+					}
+
+					if (ev == msgSysEx1 || ev == msgSysEx1 || (ev==msgMetaEvent && msg.MsgData.MetaEvent.iType==metaSequencerSpecific))
+					{
+						/* Already done a hex dump */
+					}
+					else
+					{
+						printf("\t[");
+						if (msg.bImpliedMsg) printf("%.2x!", msg.iImpliedMsg);
+						for(j=0;j<msg.iMsgSize;j++)
+							printf("%.2x ", msg.data[j]);
+						printf("]\n");
+					}
+				}
+			}
+		}
+
+		//midiReadFreeMessage(&msg);
+	}
+	else
+	{
+		printf("Open Failed!\nInvalid MIDI-File Header!\n");
+
+	}
+}
+
+
+unsigned char USART_getc(USART_TypeDef* USARTx)
+{
+	// wait until data register is empty
+	/* Wait until the data is ready to be received. */
+	while ((USARTx->SR & USART_SR_RXNE) == 0);
+
+	// read RX data, combine with DR mask (we only accept a max of 9 Bits)
+	return USARTx->DR & 0x1FF;
+}
+
+// This will print on usart 1 (the original function has been commented out in printf.c !!!
+int fputc(int ch, FILE * f)
+{
+	/* Wait until transmit finishes */
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+
+	/* Transmit the character using USART1 */
+	USART_SendData(USART1, (u8) ch);
+
+
+	return ch;
+}
+
+// receives an 32 bit integer from serial port. (little endian)
+uint32_t USART_recv_dword(USART_TypeDef* USARTx)
+{
+	uint32_t i, result = 0;
+	for(i = 0; i < 4; i++)
+	{
+		uint32_t recv = USART_getc(USART1) << (i * 8);
+		result |= recv;
+	}
+
+	return result;
+}
+
+
+//-------------------------------------------------------------------
+// Defines
+
+//---------------------------------------------------------------------------
+// Static variables
+
+//---------------------------------------------------------------------------
+// Local functions
+
+// Redirecting of printf to UARTx - this works for Atollic
+// fd selects stdout or stderr - that's UARTx or UARTd
+int _write_r(void *reent, int fd, char *ptr, size_t len) {
+	size_t counter = len;
+	USART_TypeDef *Usart;
+
+	if(fd == STDOUT_FILENO) {			// stdout goes to UARTx
+		Usart = UARTx;
+	} else if(fd == STDERR_FILENO) {
+		Usart = UARTd;					// stderr goes to UARTd
+	} else {
+		return len;
+	}
+
+	while(counter-- > 0) {				// Send the character from the buffer to UART
+		while (USART_GetFlagStatus(Usart, USART_FLAG_TXE) == RESET);
+		USART_SendData(Usart, (uint8_t) (*ptr));
+		ptr++;
+	}
+
+	return len;
+}
+
+
+/*******************************************************************************
+* Function Name  : delay
+* Description    : Inserts a time delay.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+static void delay(void)
+{
+  vu32 i = 0;
+
+  for(i = 0xFF; i != 0; i--)
+  {
+  }
+}
+
+//
+//--- Helpers for FAT support
+//
+
+FATFS Fatfs[1];		/* File system object for each logical drive */
+BYTE Buff[1*1024] __attribute__ ((aligned (4))) ;		/* Working buffer */
+char Lfname[512];
+FATFS *fs;				/* Pointer to file system object */
+DWORD acc_size;				/* Work register for fs command */
+WORD acc_files, acc_dirs;
+FILINFO Finfo;
+
+static
+void put_rc (FRESULT rc)
+{
+	const char *p;
+	static const char str[] =
+		"OK\0" "NOT_READY\0" "NO_FILE\0" "FR_NO_PATH\0" "INVALID_NAME\0" "INVALID_DRIVE\0"
+		"DENIED\0" "EXIST\0" "RW_ERROR\0" "WRITE_PROTECTED\0" "NOT_ENABLED\0"
+		"NO_FILESYSTEM\0" "INVALID_OBJECT\0" "MKFS_ABORTED\0";
+	FRESULT i;
+
+	for (p = str, i = 0; i != rc && *p; i++) {
+		while(*p++);
+	}
+	printf("rc=%u FR_%s\n", (UINT)rc, p);
+}
+
+static
+FRESULT scan_files (char* path)
+{
+	DIR dirs;
+	FRESULT res;
+	BYTE i;
+	char *fn;
+
+
+	if ((res = f_opendir(&dirs, path)) == FR_OK) {
+		i = strlen(path);
+		while (((res = f_readdir(&dirs, &Finfo)) == FR_OK) && Finfo.fname[0]) {
+#if _USE_LFN
+				fn = *Finfo.lfname ? Finfo.lfname : Finfo.fname;
+#else
+				fn = Finfo.fname;
+#endif
+			if (Finfo.fattrib & AM_DIR) {
+				acc_dirs++;
+				*(path+i) = '/'; strcpy(path+i+1, fn);
+				printf("%s\r\n", path);
+
+				res = scan_files(path);
+				*(path+i) = '\0';
+
+				if (res != FR_OK) break;
+			} else {
+
+				printf("%s/%s - %d Bytes\r\n", path, fn, Finfo.fsize);
+
+				acc_files++;
+				acc_size += Finfo.fsize;
+			}
+		}
+	}
+
+	return res;
+}
+
+//---------------------------------------------------------------------------
+// Local functions
+
+int main(void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_ClocksTypeDef RCC_ClockFreq;
+    uint32_t dw;
+    uint16_t w;
+    uint8_t b;
+    uint32_t i, j;
+
+    char file_name[64];
+    uint32_t file_size;
+
+    static uint8_t buffer[1024];
+
+	//
+	// Clock initialization
+	//
+
+	// Output SYSCLK clock on MCO pin
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	RCC_MCOConfig(RCC_MCO_SYSCLK);
+
+	//
+	// Configure debug trigger as output (GPIOA pin 0)
+	//
+
+    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	//
+	// Configure peripherals used - basically enable their clocks to enable them
+	//
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC, ENABLE);
+
+    //
+    // Configure LEDs - we use them as an indicator the platform is alive somehow
+    //
+
+    // PC8 is blue LED
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    // PC9 is green LED - some setting inherited from blue LED config code!
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    // Blue LED on - we're running!
+    GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_SET);
+
+    //
+    // GPIO initialization
+    //
+
+    // none used at the moment besides the LEDs and stuff that other peripherals configured for themselves
+
+	//
+	// UART initialization
+	//
+
+    InitializeUarts();
+
+    //
+    // Setup SysTick Timer for 1 millisecond interrupts, also enables Systick and Systick-Interrupt
+    //
+
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+        DEBUG(("Systick failed, system halted\n"));
+		while (1);
+	}
+
+    //
+    // Main program loop
+    //
+
+    // Show welcome message (From now on use USART1 when iprintf is called
+	printf("UART test " __DATE__ "\n");
+
+    RCC_GetClocksFreq(&RCC_ClockFreq);
+
+    printf("SYSCLK = %ld  HCLK = %ld  PCLK1 = %ld  PCLK2 = %ld ADCCLK = %ld\r\n", RCC_ClockFreq.SYSCLK_Frequency,
+												RCC_ClockFreq.HCLK_Frequency,
+												RCC_ClockFreq.PCLK1_Frequency,
+												RCC_ClockFreq.PCLK2_Frequency,
+												RCC_ClockFreq.ADCCLK_Frequency);
+
+
+    // SD Card Test
+    //
+    // Disk initialization - this sets SPI1 to correct mode and initializes the SD/MMC card
+    //
+
+    if(disk_initialize(0) & STA_NOINIT) {
+    	printf("No memory card found\n");
+    } else {
+        disk_ioctl(0, GET_SECTOR_COUNT, &dw);
+        disk_ioctl(0, MMC_GET_TYPE, &b);
+        if(b & CT_MMC) {
+        	printf("MMC");
+        } else if(b & CT_SD1) {
+        	printf("SD 1.0");
+        } else if(b & CT_SD2) {
+        	printf("SD 2.0");
+        } else {
+        	printf("unknown");
+        }
+        printf(" card found, %" PRIu32 "MB\n", dw >> 11);
+    }
+
+	char ptr[1024] = {0};
+
+
+	long p;
+	FRESULT res;
+    f_mount(0, &Fatfs[0]);
+
+	res = f_getfree("", (DWORD*)&p, &fs);
+	if(res) {
+		put_rc(res);
+	} else {
+		printf("FAT type = %d (%s)\nBytes/Cluster = %d\nNumber of FATs = %d\n"
+				"Root DIR entries = %d\nSectors/FAT = %d\nNumber of clusters = %d\n"
+				"FAT start (lba) = %d\nDIR start (lba,clustor) = %d\nData start (lba) = %d\n\n",
+				(WORD)fs->fs_type,
+				(fs->fs_type==FS_FAT12) ? "FAT12" : (fs->fs_type==FS_FAT16) ? "FAT16" : "FAT32",
+				(DWORD)fs->csize * 512, (WORD)fs->n_fats,
+				fs->n_rootdir, fs->fsize, (DWORD)fs->n_fatent - 2,
+				fs->fatbase, fs->dirbase, fs->database
+		);
+
+	}
+
+	acc_size = acc_files = acc_dirs = 0;
+
+
+#if _USE_LFN
+	Finfo.lfname = Lfname;
+	Finfo.lfsize = sizeof(Lfname);
+#endif
+	res = scan_files(ptr);
+	if(res) {
+		put_rc(res);
+	} else {
+		printf("%d files, %d bytes.\n%d folders.\n"
+				"%d MB total disk space.\n%d MB available.\n",
+				acc_files, acc_size, acc_dirs,
+				(fs->n_fatent - 2) * (fs->csize / 2) / 1024, p * (fs->csize / 2) / 1024
+
+		);
+	}
+
+
+
+
+	/*
+
+	FIL *fp = malloc( sizeof(FIL) );
+
+	// code for receiving a file from the serial port and writing to sd card
+	printf("waiting for file info...\n");
+
+
+	// file size of the receiving file
+	printf("file size: ");
+	file_size = USART_recv_dword(USART1);
+
+	printf("%d\n", file_size);
+
+
+	// file name of the receiving file
+	printf("file name: ");
+	i = 0;
+	do
+	{
+		b = USART_getc(USART1);
+		file_name[i] = b;
+
+		i++;
+	}
+	while (b != '\0');
+	printf("%s\n", file_name);
+
+	printf("ready to receive! now send the file!\n");
+	// create file on sd card
+	res = f_open (fp, file_name, FA_CREATE_ALWAYS | FA_WRITE);
+
+	// now receive the file!
+	uint8_t bytes_written = 0;
+	for(i = 0; i < file_size; i += 1024)
+	{
+		uint32_t chunk_size = i + 1024 < file_size ? 1024 : file_size - i;
+
+		// write every 1024 byte chunk to sd card, not every byte
+		for(j = 0; j < chunk_size; j++)
+			buffer[j] = USART_getc(USART1);
+
+		f_write(fp, buffer, chunk_size, &bytes_written); // TODO: Flowcontrol!!!
+	}
+
+	// close file
+	f_close(fp);
+
+	printf("file received successfully!!!\n");
+
+	free(fp);
+	*/
+
+	playMidiFile("/overworld_deep3.mid");
+
+	// code for reading a file and sending it to the serial port
+
+	/*
+	printf("now sending file:\n");
+
+	static FIL fp;
+	res = f_open (&fp, "/jammin.mid", FA_READ);
+
+	if (res == FR_OK)
+	{
+		static char buf[1024];
+		UINT bytes_read = 0;
+
+		for(i = 0; i < fp.fsize; i += bytes_read)
+		{
+			 res = f_read(&fp, buf, 1024, &bytes_read);
+			 if(bytes_read < 1024)
+				 bytes_read--;
+
+			 for(j = 0; j < bytes_read; j++)
+			 {
+				 while( !(USART1->SR & 0x00000040) ); // wait until data register is empty
+				 USART_SendData(USART1, buf[j]);
+			 }
+		}
+
+		f_close(&fp);
+	}
+	else
+		printf("error opening file.");
+
+
+	//free(fp);
+*/
+
+    // loop forever
+    while(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ midi/midifile.h
@@ -0,0 +1,277 @@
+#ifndef _MIDIFILE_H
+#define _MIDIFILE_H
+
+#include "ff.h"
+#include "midiinfo.h"		/* enumerations and constants for GM */
+
+/*
+ * midiFile.c -  Header file for Steevs MIDI Library
+ * Version 1.4
+ *
+ *  AUTHOR: Steven Goodwin (StevenGoodwin@gmail.com)
+ *			Copyright 1998-2010, Steven Goodwin.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License,or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* 
+** All functions start with one of the following prefixes:
+**		midiFile*		For non-GM features that relate to the file, and have
+**						no use once the file has been created, i.e. CreateFile
+**						or SetTrack (those data is embedded into the file, but
+**						not explicitly stored)
+**		midiSong*		For operations that work across the song, i.e. SetTempo
+**		midiTrack*		For operations on a specific track, i.e. AddNoteOn
+*/
+
+/*
+** Types because we're dealing with files, and need to be careful
+*/
+typedef	unsigned char		BYTE;
+typedef	unsigned short		WORD;
+typedef	unsigned long		DWORD;
+typedef int					BOOL;
+#ifndef TRUE
+#define TRUE	1
+#endif
+#ifndef FALSE
+#define FALSE	0
+#endif
+
+
+void read_mem_from_pos(void* dst, DWORD pos, DWORD length);
+void read_string_from_pos_s(void* dst, DWORD pos, DWORD max_length);
+DWORD read_dword_value_from_pos(DWORD pos);
+WORD read_word_value_from_pos(DWORD pos);
+BYTE read_byte_value_from_pos(DWORD pos);
+
+
+
+
+
+/*
+** MIDI Constants
+*/
+#define MIDI_PPQN_DEFAULT		384
+#define MIDI_VERSION_DEFAULT	1
+
+/*
+** MIDI Limits
+*/
+
+// This settings are very important for using on an embedded system!
+// When using default values, this structure will need a memory ammount of ~130KB!
+// Since the STM32 board has only 8KB RAM, this values have been limited.
+// All 256 possible midi channels aren't needed on floppy drives, so 16 should be enough.
+// Polyphony is not possible at all on floppies so the value has been changed to 4 (for overlapping tones).
+// This settings will only need 1KB instead of ~130KB
+//
+// - coon
+
+#define MAX_MIDI_TRACKS		 16 // default: 256
+#define MAX_TRACK_POLYPHONY		 4 // 64
+
+/*
+** MIDI structures, accessibly externably
+*/
+/*
+** Internal Data Structures
+*/
+typedef struct 	{
+	BYTE note, chn;
+	BYTE valid, p2;
+	DWORD end_pos;
+} MIDI_LAST_NOTE;
+
+typedef struct 	{
+	DWORD ptr2;
+	DWORD pBase2;
+	DWORD pEnd2;
+
+
+	DWORD pos;
+	DWORD dt;
+	/* For Reading MIDI Files */
+	DWORD size;						/* size of whole iTrack */
+	/* For Writing MIDI Files */
+	DWORD iBlockSize;				/* max size of track */
+	BYTE iDefaultChannel;			/* use for write only */
+	BYTE last_status;				/* used for running status */
+
+	MIDI_LAST_NOTE LastNote[MAX_TRACK_POLYPHONY];
+} MIDI_FILE_TRACK;
+
+typedef struct 	{
+	DWORD	iHeaderSize;
+	/**/
+	WORD	iVersion;		/* 0, 1 or 2 */
+	WORD	iNumTracks;		/* number of tracks... (will be 1 for MIDI type 0) */
+	WORD	PPQN;			/* pulses per quarter note */
+} MIDI_HEADER;
+
+typedef struct {
+	FIL					*pFile;
+	BOOL				bOpenForWriting;
+
+	MIDI_HEADER			Header;
+	BYTE *ptr;			/* to whole data block */
+	DWORD ptr2;
+	DWORD file_sz;
+
+	MIDI_FILE_TRACK		Track[MAX_MIDI_TRACKS];
+} _MIDI_FILE;
+
+
+//typedef	void 	_MIDI_FILE;
+typedef struct {
+					tMIDI_MSG	iType;
+
+					DWORD		dt;		/* delta time */
+					DWORD		dwAbsPos;
+					DWORD		iMsgSize;
+
+					BOOL		bImpliedMsg;
+					tMIDI_MSG	iImpliedMsg;
+
+					/* Raw data chunk */
+					BYTE data[32];		/* data block */ // delete
+					DWORD data_sz;
+					
+					union {
+						struct {
+								int			iNote;
+								int			iChannel;
+								int			iVolume;
+								} NoteOn;
+						struct {
+								int			iNote;
+								int			iChannel;
+								} NoteOff;
+						struct {
+								int			iNote;
+								int			iChannel;
+								int			iPressure;
+								} NoteKeyPressure;
+						struct {
+								int			iChannel;
+								tMIDI_CC	iControl;
+								int			iParam;
+								} NoteParameter;
+						struct {
+								int			iChannel;
+								int			iProgram;
+								} ChangeProgram;
+						struct {
+								int			iChannel;
+								int			iPressure;
+								} ChangePressure;
+						struct {
+								int			iChannel;
+								int			iPitch;
+								} PitchWheel;
+						struct {
+								tMIDI_META	iType;
+								union {
+									int					iMIDIPort;
+									int					iSequenceNumber;
+									struct {
+										//BYTE			*pData;
+										BYTE pData[256]; // 256 byte for meta events texts... (hope this is enough)
+										
+										} Text;
+									struct {
+										int				iBPM;
+										} Tempo;
+									struct {
+										int				iHours, iMins;
+										int				iSecs, iFrames,iFF;
+										} SMPTE;
+									struct {
+										tMIDI_KEYSIG	iKey;
+										} KeySig;
+									struct {
+										int				iNom, iDenom;
+										} TimeSig;
+									struct {
+										BYTE			pData[256];
+										int				iSize;
+										} Sequencer;
+									} Data;
+								} MetaEvent;
+						struct {
+								BYTE*		pData;
+								int			iSize;
+								} SysEx;
+						} MsgData;
+
+				/* State information - Please treat these as private*/
+				tMIDI_MSG	iLastMsgType;
+				BYTE		iLastMsgChnl;
+	
+				} MIDI_MSG;
+
+/*
+** midiFile* Prototypes
+*/
+// _MIDI_FILE  *midiFileCreate(const char *pFilename, BOOL bOverwriteIfExists);
+int			midiFileSetTracksDefaultChannel(_MIDI_FILE *pMF, int iTrack, int iChannel);
+int			midiFileGetTracksDefaultChannel(const _MIDI_FILE *pMF, int iTrack);
+BOOL		midiFileFlushTrack(_MIDI_FILE *pMF, int iTrack, BOOL bFlushToEnd, DWORD dwEndTimePos);
+BOOL		midiFileSyncTracks(_MIDI_FILE *pMF, int iTrack1, int iTrack2);
+int			midiFileSetPPQN(_MIDI_FILE *pMF, int PPQN);
+int			midiFileGetPPQN(const _MIDI_FILE *pMF);
+int			midiFileSetVersion(_MIDI_FILE *pMF, int iVersion);
+int			midiFileGetVersion(const _MIDI_FILE *pMF);
+void 		midiFileOpen( FIL* fil, _MIDI_FILE* pMF, const char *pFilename, BOOL* open_success );
+BOOL		midiFileClose(_MIDI_FILE *pMF);
+
+/*
+** midiSong* Prototypes
+*/
+BOOL		midiSongAddSMPTEOffset(_MIDI_FILE *pMF, int iTrack, int iHours, int iMins, int iSecs, int iFrames, int iFFrames);
+BOOL		midiSongAddSimpleTimeSig(_MIDI_FILE *pMF, int iTrack, int iNom, int iDenom);
+BOOL		midiSongAddTimeSig(_MIDI_FILE *pMF, int iTrack, int iNom, int iDenom, int iClockInMetroTick, int iNotated32nds);
+BOOL		midiSongAddKeySig(_MIDI_FILE *pMF, int iTrack, tMIDI_KEYSIG iKey);
+BOOL		midiSongAddTempo(_MIDI_FILE *pMF, int iTrack, int iTempo);
+BOOL		midiSongAddMIDIPort(_MIDI_FILE *pMF, int iTrack, int iPort);
+BOOL		midiSongAddEndSequence(_MIDI_FILE *pMF, int iTrack);
+
+/*
+** midiTrack* Prototypes
+*/
+BOOL		midiTrackAddRaw(_MIDI_FILE *pMF, int iTrack, int iDataSize, const BYTE *pData, BOOL bMovePtr, int iDeltaTime);
+BOOL		midiTrackIncTime(_MIDI_FILE *pMF, int iTrack, int iDeltaTime, BOOL bOverridePPQN);
+BOOL		midiTrackAddText(_MIDI_FILE *pMF, int iTrack, tMIDI_TEXT iType, const char *pTxt);
+BOOL		midiTrackAddMsg(_MIDI_FILE *pMF, int iTrack, tMIDI_MSG iMsg, int iParam1, int iParam2);
+BOOL		midiTrackSetKeyPressure(_MIDI_FILE *pMF, int iTrack, int iNote, int iAftertouch);
+BOOL		midiTrackAddControlChange(_MIDI_FILE *pMF, int iTrack, tMIDI_CC iCCType, int iParam);
+BOOL		midiTrackAddProgramChange(_MIDI_FILE *pMF, int iTrack, int iInstrPatch);
+BOOL		midiTrackChangeKeyPressure(_MIDI_FILE *pMF, int iTrack, int iDeltaPressure);
+BOOL		midiTrackSetPitchWheel(_MIDI_FILE *pMF, int iTrack, int iWheelPos);
+BOOL		midiTrackAddNote(_MIDI_FILE *pMF, int iTrack, int iNote, int iLength, int iVol, BOOL bAutoInc, BOOL bOverrideLength);
+BOOL		midiTrackAddRest(_MIDI_FILE *pMF, int iTrack, int iLength, BOOL bOverridePPQN);
+BOOL		midiTrackGetEndPos(_MIDI_FILE *pMF, int iTrack);
+
+/*
+** midiRead* Prototypes
+*/
+int			midiReadGetNumTracks(const _MIDI_FILE *pMF);
+BOOL		midiReadGetNextMessage(const _MIDI_FILE *pMF, int iTrack, MIDI_MSG *pMsg);
+void		midiReadInitMessage(MIDI_MSG *pMsg);
+void		midiReadFreeMessage(MIDI_MSG *pMsg);
+
+
+#endif /* _MIDIFILE_H */
+
